2de8ac2438e7ed594e6a0718d8d3c25ca01403ee
cathalsurfs <cathal@inchsurf.com>
2016-08-25 22:35:46 +0100

    Update bower component for 0.9.12 release.

--- demo/bower_components/ng-contenteditable/ng-contenteditable.js
+++ demo/bower_components/ng-contenteditable/ng-contenteditable.js
@@ -8,7 +8,7 @@ var ngContentEditable = angular.module('ngContentEditable', []);
 
 ngContentEditable.service('editable.configService', function () {
     return {
-        VERSION: '0.9.11',
+        VERSION: '0.9.12',
         DEBUG_MODE: false, // NOTE: Set false for production.
         DRAG_COMPONENT_CLASS: 'editable-component', // NOTE: This class name must be assigned to any directives working within editable regions.
         DISABLE_RANGE_OVER_EDITABLE_COMPONENT: false, // TODO: Set true for production.
@@ -183,7 +183,6 @@ ngContentEditable.directive('editable', ['$compile', 'editable.dragHelperService
                     drag.setElement(event.target);
                 })
                 .bind('dragover', function (event) {
-                    element[0].focus();
                     return true;
                 })
                 .bind('drop', function (event) {
@@ -422,7 +421,7 @@ ngContentEditable.service('editable.utilityService', ['editable.configService', 
                 var obj = new window.URL(url);
                 if (obj && obj.href) return obj.href;
             } catch (error) {
-                console.error(error);
+                console.warn(error);
             }
             return null;
         },
